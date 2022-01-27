d51f1556dd4d6892a8d6a1268cd978c17d94c6b1
Bruno Borges <Brwnb@users.noreply.github.com>
2022-01-27 17:48:36 -0300

    Update mysql_install.sh

--- mysql_install.sh
+++ mysql_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This scritp works in Debian 10 with mysql 8
+
 export PATH=$PATH:/usr/local/sbin:/sbin:/usr/sbin:/sbin
 
 clear
@@ -15,10 +17,14 @@ clear
 
 test -d /tmp/mysql_temp && echo "Não será necessário criar o diretório" && \
        	sleep 5 || mkdir /tmp/mysql_temp
+
 cd /tmp/mysql_temp
 
 pwd
 sleep 5
+
+# Link get in the official site
+# https://dev.mysql.com/downloads/repo/apt/
 
 test -f mysql*.deb && echo "Não Precisa fazer download" && sleep 5 || \
 	wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
@@ -27,5 +33,3 @@ chmod +x mysql*.deb
 dpkg -i mysql*deb
 
 apt-get update && apt-get install mysql-server -y
-
-
